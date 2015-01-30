52da98156610a4eb5366449f4a3bff15dfb9c3fc
Volodymyr Paslavskyy <foxandkamarus@gmail.com>
2015-01-30 08:46:38 -0800

    Worked witj grunt

---
+++ static/Gruntfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: {
+        version: (new Date()).getTime(),
+        name: 'app'
+    },
+    // Task configuration.
+    clean: {
+      files: ['build']
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['js/**/*.js'],
+        dest: 'build/<%= pkg.name %>.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'build/<%= pkg.name %>.min.js'
+      },
+    },
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: ['js/**/*.js']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify']);
+
+};
