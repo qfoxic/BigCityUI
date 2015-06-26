6bcb4e259aa6ee48fbb1775676abb0325b3e5e71
vpaslav <workforum@ukr.net>
2015-06-26 08:16:07 +0300

    Added search by price range

--- static/app/website/search/controller.js
+++ static/app/website/search/controller.js
@@ -12,13 +12,28 @@ angular.module('bigcity.website.search', ['ui.router'])
                     }
                 },
                 templateUrl: '/static/app/website/search/main.html',
-                controller: ['$scope', 'categories', function ($scope, categories) {
-                    $scope.categories = [];
-                    $scope.adverts = [];
-                    $scope.loading = false;
-                    $scope.data = categories;
-                    $scope.grouped = categories[2];
-                }]
+                controller: ['$scope', 'categories', 'NodesService',
+                    function ($scope, categories, NodesService) {
+                        $scope.categories = [];
+                        $scope.adverts = [];
+                        $scope.loading = false;
+                        $scope.data = categories;
+                        $scope.grouped = categories[2];
+                        $scope.search = function (params) {
+                            $scope.adverts = [];
+                            $scope.loading = true;
+                            NodesService.nearest({
+                                category: params.categoryId,
+                                priceTo: params.priceTo,
+                                priceFrom: params.priceFrom,
+                                text: params.text,
+                                location: params.location
+                            }).then(function (data) {
+                                $scope.loading = false;
+                                $scope.adverts = data.results;
+                            });
+                        };
+                    }]
             })
             .state('search.all', {
                 url: '/all',
@@ -35,7 +50,7 @@ angular.module('bigcity.website.search', ['ui.router'])
             .state('search.category', {
                 url: '/:categoryId',
                 views: {
-                    'category': {
+                    'aside': {
                         templateUrl: '/static/app/website/search/category.html',
                         controller: ['$scope', '$stateParams',
                             function ($scope, $stateParams) {
@@ -47,34 +62,26 @@ angular.module('bigcity.website.search', ['ui.router'])
                                 $scope.categories = $scope.data[2];
                                 $scope.current = curCat;
                                 $scope.parent = catDict[parentNodeId];
+                                $scope.priceTo = null;
+                                $scope.priceFrom = null;
                                 $scope.children = angular.element.grep(childNodes, function (n) {
                                     return n.parent === parentNodeId;
                                 });
                             }]
                     },
                     'search': {
-                        controller: ['$scope', '$stateParams', 'NodesService',
-                            function ($scope, $stateParams, NodesService) {
+                        templateUrl: '/static/app/website/search/search.html',
+                        controller: ['$scope', '$stateParams',
+                            function ($scope, $stateParams) {
                                 var catDict = $scope.data[3],
                                     curCat = catDict[$stateParams.categoryId],
                                     parentNodeId = curCat.parent || curCat.id;
 
                                 $scope.current = curCat;
+                                $scope.selectedCat = curCat.id;
                                 $scope.location = '';
                                 $scope.text = '';
                                 $scope.parent = catDict[parentNodeId];
-                                $scope.search = function () {
-                                    $scope.$parent.adverts = [];
-                                    $scope.$parent.loading = true;
-                                    NodesService.nearest({
-                                        category: curCat.id,
-                                        location: $scope.location,
-                                        text: $scope.text
-                                    }).then(function (data) {
-                                        $scope.$parent.loading = false;
-                                        $scope.$parent.adverts = data.results;
-                                    });
-                                };
                             }]
                     },
                     'adverts': {
@@ -85,6 +92,7 @@ angular.module('bigcity.website.search', ['ui.router'])
 
                                 $scope.$parent.loading = true;
                                 $scope.$parent.adverts = [];
+                                //TODO. Put to parent controller.
                                 NodesService.nearest({
                                     category: curCat.id
                                 }).then(function (data) {
