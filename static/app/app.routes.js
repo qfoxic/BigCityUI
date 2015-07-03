c7f8161c21dacd7aac787251e2bfb5bb5ef92f29
vpaslav <workforum@ukr.net>
2015-07-03 08:16:11 +0300

    Added advert preview

--- static/app/app.routes.js
+++ static/app/app.routes.js
@@ -1,46 +1,45 @@
 angular.module('bigcity', [
-  'bigcity.website.home',
-  'bigcity.website.search',
-  'bigcity.common.notification',
-  'bigcity.common.modal',
-  'bigcity.common.users',
-  'bigcity.common.groups',
-  'bigcity.common.nodes',
-  'bigcity.common.utils',
-  'ui.router',
-  'LocalStorageModule',
+    'bigcity.website.home',
+    'bigcity.website.search',
+    'bigcity.website.advert',
+    'bigcity.common.notification',
+    'bigcity.common.modal',
+    'bigcity.common.users',
+    'bigcity.common.groups',
+    'bigcity.common.nodes',
+    'bigcity.common.utils',
+    'ui.router',
+    'LocalStorageModule'
 ])
-.run(
-  ['$rootScope', '$state', '$stateParams', 'localStorageService', '$http', 'notify',
-  function ($rootScope, $state, $stateParams, localStorageService, $http, notify) {
-      $rootScope.curUser = localStorageService.get('usr');
-      $rootScope.cache = localStorageService;
-      $rootScope.$state = $state;
-      $rootScope.$stateParams = $stateParams;
+    .run(
+        ['$rootScope', '$state', '$stateParams', 'localStorageService', '$http',
+            function ($rootScope, $state, $stateParams, localStorageService, $http) {
+                'use strict';
+                $rootScope.curUser = localStorageService.get('usr');
+                $rootScope.cache = localStorageService;
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
 
-      if ($rootScope.curUser) {
-          $http.defaults.headers.common.Authorization = 'Token ' + $rootScope.curUser.token;
-      }
-    }
-  ]
-)
-.config(['$stateProvider', '$urlRouterProvider', 'localStorageServiceProvider',
-         '$resourceProvider',
-  function ($stateProvider, $urlRouterProvider, localStorageServiceProvider,
-      $resourceProvider) {
+                if ($rootScope.curUser) {
+                    $http.defaults.headers.common.Authorization = 'Token ' + $rootScope.curUser.token;
+                }
+            }]
+    )
+    .config(['$urlRouterProvider', 'localStorageServiceProvider', '$resourceProvider',
+        function ($urlRouterProvider, localStorageServiceProvider, $resourceProvider) {
+            'use strict';
+            localStorageServiceProvider
+                .setStorageType('sessionStorage')
+                .setNotify(false, false);
+            $resourceProvider.defaults.stripTrailingSlashes = false;
 
-    localStorageServiceProvider
-      .setStorageType('sessionStorage')
-      .setNotify(false, false);
-    $resourceProvider.defaults.stripTrailingSlashes = false;
-
-    $urlRouterProvider
-      .when('/', '/')
-      .when('/search/', '/search/')
-      .when('/login/', '/login/')
-      .when('/logout/', '/logout/')
-      .when('/groups/', '/groups/')
-      .when('/nodes/', '/nodes/')
-      .otherwise('/');
-  }
-]);
+            $urlRouterProvider
+                .when('/', '/')
+                .when('/search/', '/search/')
+                .when('/login/', '/login/')
+                .when('/logout/', '/logout/')
+                .when('/groups/', '/groups/')
+                .when('/nodes/', '/nodes/')
+                .otherwise('/');
+        }]
+);
--- static/app/website/advert/controller.js
+++ static/app/website/advert/controller.js
@@ -2,9 +2,32 @@ angular.module('bigcity.website.advert', [
     'ui.router'
 ]).config(['$stateProvider', function ($stateProvider) {
     'use strict';
-    $stateProvider.state('home', {
-        url: '/',
-        templateUrl: '/static/app/website/advert/main.html',
-        controller: ['$scope', 'NodesService', function ($scope, NodesService) {}]
-    });
+    $stateProvider
+        .state('advert', {
+            url: '/advert',
+            abstract: true,
+            templateUrl: '/static/app/website/advert/main.html'
+        })
+        .state('advert.preview', {
+            url: '/:advertId',
+            resolve: {
+                advert: function($stateParams, NodesService) {
+
+                }
+            },
+            views: {
+                'preview': {
+                    templateUrl: '/static/app/website/advert/preview.html',
+                    controller: ['$scope', 'advert', function ($scope, advert) {
+
+                    }]
+                },
+                'aside': {
+                    templateUrl: '/static/app/website/advert/aside.html',
+                    controller: ['$scope', 'advert', function ($scope, advert) {
+
+                    }]
+                }
+            }
+        });
 }]);
